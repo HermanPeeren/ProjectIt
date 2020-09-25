2e6f27243edd0652dae2c4a6caea474f00d3b4a2
Anneke Kleppe <anneke@warmerkleppe.nl>
2020-09-25 11:13:08 +0200

    reduced the octopus project to the part that work correctly and added it to the tests.

--- packages/test/src/octopus-small/__tests__/readUmlFiles.test.ts
+++ packages/test/src/octopus-small/__tests__/readUmlFiles.test.ts
@@ -16,20 +16,20 @@ function doAllFilesIn(folderPath: string) {
 describe("Testing Parser on UML files", () => {
     // TODO use snapshots
     test("book project unparsed and parsed again", () => {
-        doAllFilesIn("src/octopus/__tests__/Book-project/");
+        doAllFilesIn("src/octopus-small/__tests__/Book-project/");
     });
 
     // test skipped until PiElementReference can handle pathNames
     test.skip("dvdshop project unparsed and parsed again", () => {
-        doAllFilesIn("src/octopus/__tests__/DvdShop-project/");
+        doAllFilesIn("src/octopus-small/__tests__/DvdShop-project/");
     });
 
     test("trainWagon model unparsed and parsed again", () => {
-        doAllFilesIn("src/octopus/__tests__/Train-project/");
+        doAllFilesIn("src/octopus-small/__tests__/Train-project/");
     });
 
     test("book model STRING unparsed and parsed again", () => {
-        const langSpec: string = fs.readFileSync("src/octopus/__tests__/Book-project/Book.uml2", { encoding: "UTF8" });
+        const langSpec: string = fs.readFileSync("src/octopus-small/__tests__/Book-project/Book.uml2", { encoding: "UTF8" });
         // console.log(langSpec);
         const unit1 = reader.readFromString(langSpec, "UmlPart");
         // console.log(writer.writeToString(unit1, 0, false));
